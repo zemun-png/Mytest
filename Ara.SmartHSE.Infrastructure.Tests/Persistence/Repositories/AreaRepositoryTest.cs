9215e0fd5ad552dcf669b762f2f81a336bd1d31d
zemun-png <57329189+zemun-png@users.noreply.github.com>
2024-05-28 12:03:48 +0330

    Add files via upload

---
+++ Ara.SmartHSE.Infrastructure.Tests/Persistence/Repositories/AreaRepositoryTest.cs
@@ -0,0 +1,27 @@
+﻿namespace Ara.SmartHSE.Infrastructure.Tests.Persistence.Repositories
+{
+    public class AreaRepositoryTest
+    {
+        [Fact]
+        public async Task qweqwe()
+        {
+            //Mock< SmartHSEContext> mock=new Mock< SmartHSEContext>();
+            //var area=new Mock<DbSet<Area>>();
+            //var area1 = new Area() { Name = "0" };
+
+            ////area.Setup(s => s.AddAsync(It.IsAny<Area>())).Callback((Area a, CancellationToken token) => { })
+            //    .Returns((Area a) => ValueTask.FromResult(area1));
+
+            //mock.Setup(s=>s.Set<Area>()).Returns(area.Object);
+            //mock.Setup(s => s.SaveChangesAsync(It.IsAny<CancellationToken>())).Returns(()=>Task.CompletedTask);
+
+            //var rrrr = new AreaRepository(mock.Object);
+
+            //var res = await rrrr.AddAsync(area1);
+
+            //var r0 = 0;
+            //var dbContext = new Mock<SmartHSEContext>();
+            //employeeContextMock.Setup<DbSet<Area>>()
+        }
+    }
+}
---
+++ Ara.SmartHSE.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Ara.SmartHSE.Infrastructure.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
