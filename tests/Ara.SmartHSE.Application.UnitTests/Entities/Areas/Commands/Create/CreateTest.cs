bcc1a805ab0111804445af3093cd5e79255a267f
zemun-png <57329189+zemun-png@users.noreply.github.com>
2024-05-28 12:00:42 +0330

    Add files via upload

---
+++ tests/Ara.SmartHSE.Application.UnitTests/Entities/Areas/Commands/Create/CreateTest.cs
@@ -0,0 +1,48 @@
+﻿using Ara.SmartHSE.Application.Entities.Areas.Commands.Create;
+using Ara.SmartHSE.Application.Interfaces.Repositories.UnitOfWork;
+using Aranuma.Infrustructure.Logging;
+using Moq;
+using System.Threading;
+using Xunit;
+
+namespace Ara.SmartHSE.Application.UnitTests.Entities.Areas.Commands.Create
+{
+    public class CreateTest
+    {
+
+
+        //private readonly Mock<IUnitOfWork> _unitOfWork;
+        //private readonly Mock<ILogger> _logger;
+
+        //public CreateTest()
+        //{
+        //    _unitOfWork = new Mock<IUnitOfWork>();
+        //    _logger = new Mock<ILogger>();
+        //}
+
+
+        [Fact]
+        //[InlineData(1, 2, 3, true)]
+        //[InlineData(2, 2, 3, false)]
+        //[InlineData(51, 2, 3, false)]
+        public void AreaCreateHandlerTest()
+        {
+            //Arrange
+            //var sendData = new AreaCreateCommand()
+            //{
+
+            //};
+
+            ////Act
+            //var r = new AreaCreateHandler(_unitOfWork.Object, _logger.Object);
+
+
+            //var rr = r.Handle(sendData,CancellationToken.None);
+            var rqwe = 0;
+            //Assert
+            Assert.True(true);
+            var rrrrrrrrr = 0;
+        }
+    }
+}
+
